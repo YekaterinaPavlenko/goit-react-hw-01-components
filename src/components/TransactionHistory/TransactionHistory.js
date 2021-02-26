db082dd81c502b52c4a3d43906b444de23f12817
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-02-26 23:42:24 +0200

    добавляет уточнение проптайпов

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -28,6 +28,13 @@ const TransactionHistory = ({ items }) => {
   );
 };
 TransactionHistory.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape(PropTypes.string.isRequired)),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
 };
 export default TransactionHistory;
